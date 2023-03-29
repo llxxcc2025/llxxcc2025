3c800c6a5819d4296a5b1c1311205ebebf476ad1
llxxcc2025 <97548512+llxxcc2025@users.noreply.github.com>
2023-03-29 23:48:33 +0800

    Create lxc-gudou.php

---
+++ lxc-gudou.php
@@ -0,0 +1,59 @@
+#EXTM3U x-tvg-url=http://epg.51zmt.top:8000/cc.xml,http://epg.51zmt.top:8000/difang.xml
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV1" tvg-id="CCTV1" tvg-name="CCTV1",CCTV1
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=300&id=1402_4500
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV2" tvg-id="CCTV2" tvg-name="CCTV2",CCTV2
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=364&id=CCTV2HD_7000
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV3" tvg-id="CCTV3" tvg-name="CCTV3",CCTV3
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=322&id=1603
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV4" tvg-id="CCTV4" tvg-name="CCTV4",CCTV4
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=30&id=1604
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV5" tvg-id="CCTV5" tvg-name="CCTV5",CCTV5
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=326&id=1605
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV5" tvg-id="CCTV5" tvg-name="CCTV5",CCTV5+
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=302&id=CCTV5plusHD_7000
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV6" tvg-id="CCTV6" tvg-name="CCTV6",CCTV6
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=328&id=1606
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV7" tvg-id="CCTV7" tvg-name="CCTV7",CCTV7
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=366&id=1607
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV8" tvg-id="CCTV8" tvg-name="CCTV8",CCTV8
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=324&id=1608
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV9" tvg-id="CCTV9" tvg-name="CCTV9",CCTV9
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=368&id=1609
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV10" tvg-id="CCTV10" tvg-name="CCTV10",CCTV10
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=370&id=CCTV10HD_7000
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV11" tvg-id="CCTV11" tvg-name="CCTV11",CCTV11
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=44&id=CCTV-11HD_7500
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV12" tvg-id="CCTV12" tvg-name="CCTV12",CCTV12
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=372&id=CCTV12HD_7000
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV13" tvg-id="CCTV13" tvg-name="CCTV13",CCTV13
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=38&id=1613
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV14" tvg-id="CCTV14" tvg-name="CCTV14",CCTV14
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=374&id=CCTV14HD_7000
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV15" tvg-id="CCTV15" tvg-name="CCTV15",CCTV15
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=48&id=CCTV-15HD_7500
+#EXTINF:-1 tvg-logo="https://wtv.tools.bianbingdang.com/prod/api/v1/tvg/logo/CCTV16" tvg-id="CCTV16" tvg-name="CCTV16",CCTV16
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1534&id=1617_4500
+#EXTINF:-1,CGTN
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=52&id=CCTV15news_1500
+#EXTINF:-1,广州共享课堂一年级
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1330&id=2061_1500
+#EXTINF:-1,广州共享课堂二年级
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1332&id=2062_1500
+#EXTINF:-1,广州共享课堂三年级
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1340&id=2063_1500
+#EXTINF:-1,广州共享课堂四年级
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1336&id=2064_1500
+#EXTINF:-1,广州共享课堂五年级
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1338&id=2065_1500
+#EXTINF:-1,广州共享课堂六年级
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1340&id=2066_1500
+#EXTINF:-1,广州共享课堂初一
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1342&id=2067_1500
+#EXTINF:-1,广州共享课堂初二
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1344&id=2068_1500
+#EXTINF:-1,广州共享课堂初三
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1346&id=2069_1500
+#EXTINF:-1,广州共享课堂高一
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1348&id=2070_1500
+#EXTINF:-1,广州共享课堂高二
+http://192.168.100.1:4567/gudou.php?phone=13389247903&ptoken=rXL2iRf80e2%2Ff4hiIfV0AQ%3D%3D&pserialnumber=a6cafa9caf5510c6&cid=1350&id=2071_1500
