713c28f32b5f605272f1c4266c76ffc5295f67f6
llxxcc2025 <97548512+llxxcc2025@users.noreply.github.com>
2025-10-12 22:29:20 +0800

    Create hbgd.php

---
+++ hbgd.php
@@ -0,0 +1,139 @@
+#EXTM3U x-tvg-url="https://epg.tv.darwinchow.com/epg.xml.gz" catchup="append" catchup-source="&playback=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"
+#EXTINF:-1,tvg-id="CCTV1" tvg-name="CCTV1" tvg-logo="https://epg.112114.xyz/logo/CCTV1.png" group-title="央视",CCTV1综合
+https://vcstiok.serv00.net/hbgd.php?id=cctv1hd
+#EXTINF:-1,tvg-id="CCTV2" tvg-name="CCTV2" tvg-logo="https://epg.112114.xyz/logo/CCTV2.png" group-title="央视",CCTV2财经
+https://vcstiok.serv00.net/hbgd.php?id=cctv2hd
+#EXTINF:-1,tvg-id="CCTV3" tvg-name="CCTV3" tvg-logo="https://epg.112114.xyz/logo/CCTV3.png" group-title="央视",CCTV3综艺
+https://vcstiok.serv00.net/hbgd.php?id=cctv3hd
+#EXTINF:-1,tvg-id="CCTV4" tvg-name="CCTV4" tvg-logo="https://epg.112114.xyz/logo/CCTV4.png" group-title="央视",CCTV4国际
+https://vcstiok.serv00.net/hbgd.php?id=cctv4
+#EXTINF:-1,tvg-id="CCTV5" tvg-name="CCTV5" tvg-logo="https://epg.112114.xyz/logo/CCTV5.png" group-title="央视",CCTV5体育
+https://vcstiok.serv00.net/hbgd.php?id=cctv5hd
+#EXTINF:-1,tvg-id="CCTV5+" tvg-name="CCTV5+" tvg-logo="https://epg.112114.xyz/logo/CCTV5+.png" group-title="央视",CCTV5+体育赛事
+https://vcstiok.serv00.net/hbgd.php?id=cctv5jhd
+#EXTINF:-1,tvg-id="CCTV6" tvg-name="CCTV6" tvg-logo="https://epg.112114.xyz/logo/CCTV6.png" group-title="央视",CCTV6电影
+https://vcstiok.serv00.net/hbgd.php?id=cctv6hd
+#EXTINF:-1,tvg-id="CCTV7" tvg-name="CCTV7" tvg-logo="https://epg.112114.xyz/logo/CCTV7.png" group-title="央视",CCTV7国防军事
+https://vcstiok.serv00.net/hbgd.php?id=cctv7hd
+#EXTINF:-1,tvg-id="CCTV8" tvg-name="CCTV8" tvg-logo="https://epg.112114.xyz/logo/CCTV8.png" group-title="央视",CCTV8电视剧
+https://vcstiok.serv00.net/hbgd.php?id=cctv8hd
+#EXTINF:-1,tvg-id="CCTV9" tvg-name="CCTV9" tvg-logo="https://epg.112114.xyz/logo/CCTV9.png" group-title="央视",CCTV9纪录
+https://vcstiok.serv00.net/hbgd.php?id=cctv9hd
+#EXTINF:-1,tvg-id="CCTV10" tvg-name="CCTV10" tvg-logo="https://epg.112114.xyz/logo/CCTV10.png" group-title="央视",CCTV10科教
+https://vcstiok.serv00.net/hbgd.php?id=cctv10hd
+#EXTINF:-1,tvg-id="CCTV11" tvg-name="CCTV11" tvg-logo="https://epg.112114.xyz/logo/CCTV11.png" group-title="央视",CCTV11戏曲
+https://vcstiok.serv00.net/hbgd.php?id=cctv11
+#EXTINF:-1,tvg-id="CCTV12" tvg-name="CCTV12" tvg-logo="https://epg.112114.xyz/logo/CCTV12.png" group-title="央视",CCTV12社会与法
+https://vcstiok.serv00.net/hbgd.php?id=cctv12hd
+#EXTINF:-1,tvg-id="CCTV13" tvg-name="CCTV13" tvg-logo="https://epg.112114.xyz/logo/CCTV13.png" group-title="央视",CCTV13新闻
+https://vcstiok.serv00.net/hbgd.php?id=cctv13
+#EXTINF:-1,tvg-id="CCTV14" tvg-name="CCTV14" tvg-logo="https://epg.112114.xyz/logo/CCTV14.png" group-title="央视",CCTV14少儿
+https://vcstiok.serv00.net/hbgd.php?id=cctv14hd
+#EXTINF:-1,tvg-id="CCTV15" tvg-name="CCTV15" tvg-logo="https://epg.112114.xyz/logo/CCTV15.png" group-title="央视",CCTV15音乐
+https://vcstiok.serv00.net/hbgd.php?id=cctv15
+#EXTINF:-1,tvg-id="CCTV16" tvg-name="CCTV16" tvg-logo="https://epg.112114.xyz/logo/CCTV16.png" group-title="央视",CCTV16奥林匹克
+https://vcstiok.serv00.net/hbgd.php?id=cctv16hd
+#EXTINF:-1,tvg-id="CCTV17" tvg-name="CCTV17" tvg-logo="https://epg.112114.xyz/logo/CCTV17.png" group-title="央视",CCTV17农业农村
+https://vcstiok.serv00.net/hbgd.php?id=cctv17hd
+#EXTINF:-1,tvg-id="湖北卫视" tvg-name="湖北卫视" tvg-logo="https://epg.112114.xyz/logo/湖北卫视.png" group-title="卫视",湖北卫视
+https://vcstiok.serv00.net/hbgd.php?id=hbwshd
+#EXTINF:-1,tvg-id="湖南卫视" tvg-name="湖南卫视" tvg-logo="https://epg.112114.xyz/logo/湖南卫视.png" group-title="卫视",湖南卫视
+https://vcstiok.serv00.net/hbgd.php?id=hnwshd
+#EXTINF:-1,tvg-id="东方卫视" tvg-name="东方卫视" tvg-logo="https://epg.112114.xyz/logo/东方卫视.png" group-title="卫视",东方卫视
+https://vcstiok.serv00.net/hbgd.php?id=dfwshd
+#EXTINF:-1,tvg-id="浙江卫视" tvg-name="浙江卫视" tvg-logo="https://epg.112114.xyz/logo/浙江卫视.png" group-title="卫视",浙江卫视
+https://vcstiok.serv00.net/hbgd.php?id=zjwshd
+#EXTINF:-1,tvg-id="北京卫视" tvg-name="北京卫视" tvg-logo="https://epg.112114.xyz/logo/北京卫视.png" group-title="卫视",北京卫视
+https://vcstiok.serv00.net/hbgd.php?id=bjwshd
+#EXTINF:-1,tvg-id="江苏卫视" tvg-name="江苏卫视" tvg-logo="https://epg.112114.xyz/logo/江苏卫视.png" group-title="卫视",江苏卫视
+https://vcstiok.serv00.net/hbgd.php?id=jswshd
+#EXTINF:-1,tvg-id="安徽卫视" tvg-name="安徽卫视" tvg-logo="https://epg.112114.xyz/logo/安徽卫视.png" group-title="卫视",安徽卫视
+https://vcstiok.serv00.net/hbgd.php?id=ahwshd
+#EXTINF:-1,tvg-id="山东卫视" tvg-name="山东卫视" tvg-logo="https://epg.112114.xyz/logo/山东卫视.png" group-title="卫视",山东卫视
+https://vcstiok.serv00.net/hbgd.php?id=sdwdhd
+#EXTINF:-1,tvg-id="河南卫视" tvg-name="河南卫视" tvg-logo="https://epg.112114.xyz/logo/河南卫视.png" group-title="卫视",河南卫视
+https://vcstiok.serv00.net/hbgd.php?id=henwshd
+#EXTINF:-1,tvg-id="四川卫视" tvg-name="四川卫视" tvg-logo="https://epg.112114.xyz/logo/四川卫视.png" group-title="卫视",四川卫视
+https://vcstiok.serv00.net/hbgd.php?id=scwshd
+#EXTINF:-1,tvg-id="河北卫视" tvg-name="河北卫视" tvg-logo="https://epg.112114.xyz/logo/河北卫视.png" group-title="卫视",河北卫视
+https://vcstiok.serv00.net/hbgd.php?id=hebws
+#EXTINF:-1,tvg-id="吉林卫视" tvg-name="吉林卫视" tvg-logo="https://epg.112114.xyz/logo/吉林卫视.png" group-title="卫视",吉林卫视
+https://vcstiok.serv00.net/hbgd.php?id=jlwshd
+#EXTINF:-1,tvg-id="黑龙江卫视" tvg-name="黑龙江卫视" tvg-logo="https://epg.112114.xyz/logo/黑龙江卫视.png" group-title="卫视",黑龙江卫视
+https://vcstiok.serv00.net/hbgd.php?id=hljwshd
+#EXTINF:-1,tvg-id="辽宁卫视" tvg-name="辽宁卫视" tvg-logo="https://epg.112114.xyz/logo/辽宁卫视.png" group-title="卫视",辽宁卫视
+https://vcstiok.serv00.net/hbgd.php?id=lnwshd
+#EXTINF:-1,tvg-id="广东卫视" tvg-name="广东卫视" tvg-logo="https://epg.112114.xyz/logo/广东卫视.png" group-title="卫视",广东卫视
+https://vcstiok.serv00.net/hbgd.php?id=gdwshd
+#EXTINF:-1,tvg-id="深圳卫视" tvg-name="深圳卫视" tvg-logo="https://epg.112114.xyz/logo/深圳卫视.png" group-title="卫视",深圳卫视
+https://vcstiok.serv00.net/hbgd.php?id=szwshd
+#EXTINF:-1,tvg-id="重庆卫视" tvg-name="重庆卫视" tvg-logo="https://epg.112114.xyz/logo/重庆卫视.png" group-title="卫视",重庆卫视
+https://vcstiok.serv00.net/hbgd.php?id=cqwshd
+#EXTINF:-1,tvg-id="大宁卫视" tvg-name="大宁卫视" tvg-logo="https://epg.112114.xyz/logo/大宁卫视.png" group-title="卫视",大宁卫视
+https://vcstiok.serv00.net/hbgd.php?id=dnwshd
+#EXTINF:-1,tvg-id="广西卫视" tvg-name="广西卫视" tvg-logo="https://epg.112114.xyz/logo/广西卫视.png" group-title="卫视",广西卫视
+https://vcstiok.serv00.net/hbgd.php?id=gxwshd
+#EXTINF:-1,tvg-id="贵州卫视" tvg-name="贵州卫视" tvg-logo="https://epg.112114.xyz/logo/贵州卫视.png" group-title="卫视",贵州卫视
+https://vcstiok.serv00.net/hbgd.php?id=gzwshd
+#EXTINF:-1,tvg-id="江西卫视" tvg-name="江西卫视" tvg-logo="https://epg.112114.xyz/logo/江西卫视.png" group-title="卫视",江西卫视
+https://vcstiok.serv00.net/hbgd.php?id=jxwshd
+#EXTINF:-1,tvg-id="天津卫视" tvg-name="天津卫视" tvg-logo="https://epg.112114.xyz/logo/天津卫视.png" group-title="卫视",天津卫视
+https://vcstiok.serv00.net/hbgd.php?id=tjwshd
+#EXTINF:-1,tvg-id="云南卫视" tvg-name="云南卫视" tvg-logo="https://epg.112114.xyz/logo/云南卫视.png" group-title="卫视",云南卫视
+https://vcstiok.serv00.net/hbgd.php?id=ynws
+#EXTINF:-1,tvg-id="山西卫视" tvg-name="山西卫视" tvg-logo="https://epg.112114.xyz/logo/山西卫视.png" group-title="卫视",山西卫视
+https://vcstiok.serv00.net/hbgd.php?id=sxws
+#EXTINF:-1,tvg-id="陕西卫视" tvg-name="陕西卫视" tvg-logo="https://epg.112114.xyz/logo/陕西卫视.png" group-title="卫视",陕西卫视
+https://vcstiok.serv00.net/hbgd.php?id=shxws
+#EXTINF:-1,tvg-id="甘肃卫视" tvg-name="甘肃卫视" tvg-logo="https://epg.112114.xyz/logo/甘肃卫视.png" group-title="卫视",甘肃卫视
+https://vcstiok.serv00.net/hbgd.php?id=gsws
+#EXTINF:-1,tvg-id="宁夏卫视" tvg-name="宁夏卫视" tvg-logo="https://epg.112114.xyz/logo/宁夏卫视.png" group-title="卫视",宁夏卫视
+https://vcstiok.serv00.net/hbgd.php?id=nxws
+#EXTINF:-1,tvg-id="内蒙古卫视" tvg-name="内蒙古卫视" tvg-logo="https://epg.112114.xyz/logo/内蒙古卫视.png" group-title="卫视",内蒙古卫视
+https://vcstiok.serv00.net/hbgd.php?id=nmws
+#EXTINF:-1,tvg-id="青海卫视" tvg-name="青海卫视" tvg-logo="https://epg.112114.xyz/logo/青海卫视.png" group-title="卫视",青海卫视
+https://vcstiok.serv00.net/hbgd.php?id=qhws
+#EXTINF:-1,tvg-id="海南卫视" tvg-name="海南卫视" tvg-logo="https://epg.112114.xyz/logo/海南卫视.png" group-title="卫视",海南卫视
+https://vcstiok.serv00.net/hbgd.php?id=hainwshd
+#EXTINF:-1,tvg-id="新疆卫视" tvg-name="新疆卫视" tvg-logo="https://epg.112114.xyz/logo/新疆卫视.png" group-title="卫视",新疆卫视
+https://vcstiok.serv00.net/hbgd.php?id=xjws
+#EXTINF:-1,tvg-id="西藏卫视" tvg-name="西藏卫视" tvg-logo="https://epg.112114.xyz/logo/西藏卫视.png" group-title="卫视",西藏卫视
+https://vcstiok.serv00.net/hbgd.php?id=xzws
+#EXTINF:-1,tvg-id="兵团卫视" tvg-name="兵团卫视" tvg-logo="https://epg.112114.xyz/logo/兵团卫视.png" group-title="卫视",兵团卫视
+https://vcstiok.serv00.net/hbgd.php?id=btws
+#EXTINF:-1,tvg-id="湖北综合" tvg-name="湖北综合" tvg-logo="https://epg.112114.xyz/logo/湖北综合.png" group-title="湖北",湖北综合频道
+https://vcstiok.serv00.net/hbgd.php?id=hbzhhd
+#EXTINF:-1,tvg-id="湖北经视" tvg-name="湖北经视" tvg-logo="https://epg.112114.xyz/logo/湖北经视.png" group-title="湖北",湖北经视
+https://vcstiok.serv00.net/hbgd.php?id=hbjsjd
+#EXTINF:-1,tvg-id="湖北公共·新闻" tvg-name="湖北公共·新闻" tvg-logo="https://epg.112114.xyz/logo/湖北公共·新闻.png" group-title="湖北",湖北公共频道
+https://vcstiok.serv00.net/hbgd.php?id=hbgghd
+#EXTINF:-1,tvg-id="湖北教育" tvg-name="湖北教育" tvg-logo="https://epg.112114.xyz/logo/湖北教育.png" group-title="湖北",湖北教育频道
+https://vcstiok.serv00.net/hbgd.php?id=hbjy
+#EXTINF:-1,tvg-id="湖北生活" tvg-name="湖北生活" tvg-logo="https://epg.112114.xyz/logo/湖北生活.png" group-title="湖北",湖北生活频道
+https://vcstiok.serv00.net/hbgd.php?id=hbsh
+#EXTINF:-1,tvg-id="湖北影视" tvg-name="湖北影视" tvg-logo="https://epg.112114.xyz/logo/湖北影视.png" group-title="湖北",湖北影视频道
+https://vcstiok.serv00.net/hbgd.php?id=hbys
+#EXTINF:-1,tvg-id="中国教育1台" tvg-name="中国教育1台" tvg-logo="https://epg.112114.xyz/logo/中国教育1台.png" group-title="其他",中国教育1台
+https://vcstiok.serv00.net/hbgd.php?id=zgjy1hd
+#EXTINF:-1,tvg-id="中国教育4台" tvg-name="中国教育4台" tvg-logo="https://epg.112114.xyz/logo/中国教育4台.png" group-title="其他",中国教育4台
+https://vcstiok.serv00.net/hbgd.php?id=zgjy4
+#EXTINF:-1,tvg-id="广东经济科教" tvg-name="广东经济科教" tvg-logo="https://epg.112114.xyz/logo/广东经济科教.png" group-title="其他",经济科教
+https://vcstiok.serv00.net/hbgd.php?id=jjkt
+#EXTINF:-1,tvg-id="家家健康" tvg-name="家家健康" tvg-logo="https://epg.112114.xyz/logo/家家健康.png" group-title="其他",家家健康
+https://vcstiok.serv00.net/hbgd.php?id=jiajkt
+#EXTINF:-1,tvg-id="档案纪实" tvg-name="档案纪实" tvg-logo="https://epg.112114.xyz/logo/档案纪实.png" group-title="其他",档案纪实
+https://vcstiok.serv00.net/hbgd.php?id=dajs
+#EXTINF:-1,tvg-id="教育纪实" tvg-name="教育纪实" tvg-logo="https://epg.112114.xyz/logo/教育纪实.png" group-title="其他",教育纪实
+https://vcstiok.serv00.net/hbgd.php?id=jyjs
+#EXTINF:-1,tvg-id="CCTV新闻国际" tvg-name="CCTV新闻国际" tvg-logo="https://epg.112114.xyz/logo/CCTV新闻国际.png" group-title="央视",CCTV新闻国际
+https://vcstiok.serv00.net/hbgd.php?id=cctvnews
+#EXTINF:-1,tvg-id="交通旅游" tvg-name="交通旅游" tvg-logo="https://epg.112114.xyz/logo/交通旅游.png" group-title="其他",交通旅游
+https://vcstiok.serv00.net/hbgd.php?id=jtlc
+#EXTINF:-1,tvg-id="卡酷少儿" tvg-name="卡酷少儿" tvg-logo="https://epg.112114.xyz/logo/卡酷少儿.png" group-title="其他",卡酷少儿
+https://vcstiok.serv00.net/hbgd.php?id=kkse
+#EXTINF:-1,tvg-id="生活剧场" tvg-name="生活剧场" tvg-logo="https://epg.112114.xyz/logo/生活剧场.png" group-title="其他",生活剧场
+https://vcstiok.serv00.net/hbgd.php?id=shjs
+#EXTINF:-1,tvg-id="中国气象" tvg-name="中国气象" tvg-logo="https://epg.112114.xyz/logo/中国气象.png" group-title="其他",中国气象
+https://vcstiok.serv00.net/hbgd.php?id=zgqx
+#EXTINF:-1,tvg-id="山东教育卫视" tvg-name="山东教育卫视" tvg-logo="https://epg.112114.xyz/logo/山东教育卫视.png" group-title="卫视",山东教育卫视
+https://vcstiok.serv00.net/hbgd.php?id=sdjyws
